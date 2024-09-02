113c387be0a5d21e23a0753c4d3f1825939bcc6d
vedasole <vedasole001@gmail.com>
2024-09-02 12:51:51 +0530

    chore: Update dependencies for @vercel/analytics and @vercel/speed-insights

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Portfolio from "./pages/portfolio";
 import Header from './pages/header';
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
         <Header />
         <Portfolio />
       </div>
+      <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   );
 }
