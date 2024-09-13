24f3f170a3d1b4a8b1ea87e2d91dea14bc5f63de
vedasole <vedasole001@gmail.com>
2024-09-14 04:17:09 +0530

    refactor: Update project card grid gap and styles in portfolio section with resized images

--- components/portfolio/projects.jsx
+++ components/portfolio/projects.jsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import ShineBorder from "../magicui/shine-border";
 // import Particles from '@/components/magicui/particles';
 import { useTheme } from 'next-themes';
+import BlurFade from "../magicui/blur-fade";
 // import { useState, useEffect } from 'react';
 
 export default function Projects() {
@@ -22,82 +23,91 @@ export default function Projects() {
     {
       id: 1,
       name: 'EKart Shopping',
-      description: 'An E-Commerce App',
+      title: 'An E-Commerce App',
+      description: 'An E-Commerce App made with React, Java, Spring Boot, AWS',
       image: '/ekart.jpeg',
       website: 'https://ekart.vedasole.cloud/',
       github: 'https://github.com/ved-asole/eKart-ecommerce-app'
     },
     {
       id: 2,
-      name: 'Project 2',
-      description: 'A brief description of the project',
-      image: '/ekart.jpeg',
+      name: 'Blog Application API',
+      title: 'An Blog Appication Backend',
+      description: 'An Blog Appication API made with Java, Spring Boot, AWS RDS and MYSQL',
+      image: '/blog-application-api.png',
       website: 'https://ekart.vedasole.cloud/',
-      github: 'https://github.com/ved-asole/eKart-ecommerce-app'
+      github: 'https://github.com/ved-asole/blog-application-api'
     },
     {
       id: 3,
-      name: 'Project 3',
-      description: 'A brief description of the project',
-      image: '/ekart.jpeg',
+      name: 'Ved Veera Academy',
+      title: 'An Online Teaching Academy',
+      description: 'An Online Teaching Academy made with Java, Spring Boot and Thymeleaf',
+      image: '/vedveera-academy.png',
       website: 'https://ekart.vedasole.cloud/',
-      github: 'https://github.com/ved-asole/eKart-ecommerce-app'
+      github: 'https://github.com/ved-asole/ved-veera-academy'
     },
     {
       id: 4,
       name: 'Project 4',
-      description: 'A brief description of the project',
-      image: '/ekart.jpeg',
+      title: 'Project 4 Title',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      image: '/blog-application-api.png',
       website: 'https://ekart.vedasole.cloud/',
-      github: 'https://github.com/ved-asole/eKart-ecommerce-app'
+      github: 'https://github.com/ved-asole/blog-application-api'
     }
   ]
 
   return (
     <section id="projects" className="flex min-h-screen items-center justify-center bg-background px-4 py-16">
       <div className="container">
-        <h2 className="mb-8 text-3xl font-bold md:text-4xl">Projects</h2>
+        <BlurFade delay={0.25} inView>
+          <h2 className="mb-8 text-3xl font-bold md:text-4xl">Projects</h2>
+        </BlurFade>
         <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4">
           {
             projects.map((project) => (
-              <ShineBorder key={project.id}
-                // className="text-center text-2xl font-bold capitalize mt-20 p-0"
-                className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl md:mt-20 p-0"
-                color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
-              >
-                <Card className="flex z-5 flex-col border-none" >
-                  <CardHeader>
-                    <CardTitle>{project.name}</CardTitle>
-                    <CardDescription>{project.description}</CardDescription>
-                  </CardHeader>
-                  <CardContent>
-                    <Image
-                      src={project.image}
-                      alt={project.name}
-                      className="mb-4 w-full rounded-lg object-cover"
-                      height={200}
-                      width={200}
-                    />
-                    <p className="text-sm text-muted-foreground mt-2">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                  </CardContent>
-                  <CardFooter className="flex justify-between">
-                    <Link href={project.website}>
-                      <Button variant="outline" size="sm">
-                        <ExternalLink className="mr-2 h-4 w-4" />
-                        Website
-                      </Button>
-                    </Link>
-                    <Link href={project.github}>
-                      <Button variant="outline" size="sm">
-                        <Github className="mr-2 h-4 w-4" />
-                        GitHub
-                      </Button>
-                    </Link>
-                  </CardFooter>
-                </Card>
-              </ShineBorder>
+              <BlurFade delay={0.50} key={project.id} inView>
+                <ShineBorder
+                  // className="text-center text-2xl font-bold capitalize mt-20 p-0"
+                  className="relative flex h-[500px] w-full shadow-sm flex-col items-center justify-center overflow-hidden rounded-lg border bg-card 
+                md:shadow-xl md:mt-20 p-0"
+                  color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+                >
+                  <Card className="flex z-10 flex-col bg-transparent shadow-none border-0" >
+                    <CardHeader>
+                      <CardTitle>{project.name}</CardTitle>
+                      <CardDescription>{project.title}</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                      <Image
+                        src={project.image}
+                        alt={project.name}
+                        className="mb-4 mx-auto rounded-lg object-cover"
+                        height={160}
+                        width={240}
+                      />
+                      <p className="text-sm text-muted-foreground mt-2">
+                        {project.description}
+                      </p>
+                    </CardContent>
+                    <CardFooter className="flex justify-between">
+                      <Link href={project.website}>
+                        <Button variant="outline" size="sm">
+                          <ExternalLink className="mr-2 h-4 w-4" />
+                          Website
+                        </Button>
+                      </Link>
+                      <Link href={project.github}>
+                        <Button variant="outline" size="sm">
+                          <Github className="mr-2 h-4 w-4" />
+                          GitHub
+                        </Button>
+                      </Link>
+                    </CardFooter>
+                  </Card>
+                </ShineBorder>
+              </BlurFade>
             ))}
         </div>
       </div >
